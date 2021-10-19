170391dce5381eed8f0161bfcdbad49366f421d2
mawavial <panzerkovic@gmail.com>
2021-10-19 12:33:34 -0300

    add new stuff

--- back/test/app/controller/todoController.spec.js
+++ back/test/app/controller/todoController.spec.js
@@ -1,13 +1,13 @@
-const jasmine = require('jasmine')
-const todoModel = require('../../app/')
-describe('todo controller', () => { 
-    it('should create a todo', () => { 
-        const todoModel({
-            name: 'what to do',
-            description: 'do some stuff before midnight'
-        });
-        result = await todoBusiness.createTodo(todomodel);
-        expect
+// const jasmine = require('jasmine')
+// const todoModel = require('../../app/')
+// describe('todo controller', () => { 
+//     it('should create a todo', () => { 
+//         const todoModel({
+//             name: 'what to do',
+//             description: 'do some stuff before midnight'
+//         });
+//         result = await todoBusiness.createTodo(todomodel);
+//         expect
 
-    })
-})
+//     })
+// })
